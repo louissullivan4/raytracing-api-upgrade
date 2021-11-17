6a6676e438e12bb5607860a9c6a11c889d1aa71a
louissullivan4 <sullivanlouis0@gmail.com>
2021-11-17 17:43:49 +0000

    created the api class file with the new lists and camera object

---
+++ src/raytracer/api.java
@@ -0,0 +1,21 @@
+package raytracer;
+
+import raytracer.*;
+import raytracer.pigments.Finish;
+import raytracer.pigments.Pigment;
+import raytracer.shapes.Shape;
+
+import java.util.ArrayList;
+
+
+public class api {
+
+
+    private Camera camera;
+    private final ArrayList<Light> lightsList = new ArrayList<>();
+    private final ArrayList<Pigment> pigmentsList = new ArrayList<>();
+    private final ArrayList<Finish> finishesList = new ArrayList<>();
+    private final ArrayList<Shape> shapesList = new ArrayList<>();
+
+
+}
